1d36199e1a876641c8c6337d9dc09c22622b4a3e
EdgeKami <38590783+AkposCodex@users.noreply.github.com>
2021-06-13 22:21:40 +0100

    site hosting update

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,9 +1,23 @@
 import { Component, OnInit } from '@angular/core';
+import { trigger, state, style, animate, transition } from "@angular/animations";
 
 @Component({
   selector: 'app-home',
+  styleUrls: ['./home.component.scss'],
   templateUrl: './home.component.html',
-  styleUrls: ['./home.component.scss']
+  animations: [
+    trigger('swing', [
+      state('start', style({
+        display: 'block',
+      })),
+      state('stop', style({
+        display: 'none',
+      })),
+      transition('start <=> stop', [
+        animate('0.5s 200ms ease-in')
+      ]),
+    ])
+  ],
 })
 export class HomeComponent implements OnInit {
 
